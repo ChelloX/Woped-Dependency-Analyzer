b674c35f8f568997fc6e11d1575e05ac02a29161
ChelloX <d.semling@gmx.de>
2020-05-06 13:15:01 +0200

    Edit gitignore + Remove Path from App

--- src/main/java/woped_dependency_visualizer/App.java
+++ src/main/java/woped_dependency_visualizer/App.java
@@ -3,6 +3,6 @@ package woped_dependency_visualizer;
 public class App {
 	public static void main(String args[]) {
 		Visualizer v = new Visualizer();
-		v.doSth("F:\\Daniel\\DHBW\\6.Semester\\Integrationsseminar\\001DEV\\woped-sf\\woped-code");
+		v.doSth(""); //Woped-Dir
 	}
 }
