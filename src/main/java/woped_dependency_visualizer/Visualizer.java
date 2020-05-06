30fa3e00db484893d9a30559dabf0e43bcc705f3
ChelloX <d.semling@gmx.de>
2020-05-06 15:03:59 +0200

    Remove unused imports

--- src/main/java/woped_dependency_visualizer/Visualizer.java
+++ src/main/java/woped_dependency_visualizer/Visualizer.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
